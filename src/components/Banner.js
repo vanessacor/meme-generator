21570011bfe2fe9247f54e6097f27a15c8e2eeeb
Vanessacor <vanessapcbarros@gmail.com>
2020-04-09 15:36:52 +0200

    add banner element

---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div class="banner-wrapper">
+      <div class="banner">
+        <a href="https://vanessacor.github.io/site/">
+          Vanessa
+          <br />
+          Poppe
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
