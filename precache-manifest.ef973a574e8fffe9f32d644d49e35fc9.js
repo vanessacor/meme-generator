29bbf29ae5152e81163f2d3210e1b0a7c0d92e7b
Vanessacor <vanessapcbarros@gmail.com>
2020-04-09 14:44:06 +0200

    Updates

---
+++ precache-manifest.ef973a574e8fffe9f32d644d49e35fc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211418def6efa8faf93d069eba99f018",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "86f882f0b1f4b25b2c8d",
+    "url": "/meme-generator/static/css/main.7fe82a43.chunk.css"
+  },
+  {
+    "revision": "1076d69a41b7c98e2890",
+    "url": "/meme-generator/static/js/2.dc4f953d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.dc4f953d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f882f0b1f4b25b2c8d",
+    "url": "/meme-generator/static/js/main.2bf5f17b.chunk.js"
+  },
+  {
+    "revision": "47ef1aee987066625a71",
+    "url": "/meme-generator/static/js/runtime-main.bc1ff169.js"
+  },
+  {
+    "revision": "32c9da0b41ec934c8e4f2b6e7fee1020",
+    "url": "/meme-generator/static/media/ImageHeader.32c9da0b.png"
+  }
+]);
